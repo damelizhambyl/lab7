87182ddfae83ca43ed80aee1ea91059bc019c236
damelizhambyl <damelizhambyl@gmail.com>
2025-03-15 08:33:03 +0500

    clock.py

--- lab7/clock.py
+++ lab7/clock.py
@@ -26,7 +26,7 @@ while not done:
     minute = current_time.tm_min
     second = current_time.tm_sec
     
-    #минут пен сикундтың бұрышын алып аламыз
+    #минут пен секундтың бұрышын алып аламыз
     #қазіргі минут * 360 градус / 60 минут + қазіргі секундты қосамыз 
     minute_angle = minute * 6+ (second / 60) * 6   
     second_angle = second * 6  
